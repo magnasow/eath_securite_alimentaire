556b7ef30f4ab91459c7c8335c8bb16ce368d8a3
magnasow <sowmarie292@gmail.com>
2024-08-09 15:52:13 +0200

    Update ProduitIngredientKey.java

--- backend/src/main/java/com/eath/entite/ProduitIngredientKey.java
+++ backend/src/main/java/com/eath/entite/ProduitIngredientKey.java
@@ -1,5 +1,6 @@
 package com.eath.entite;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import java.io.Serializable;
 import java.util.Objects;
@@ -7,7 +8,10 @@ import java.util.Objects;
 @Embeddable
 public class ProduitIngredientKey implements Serializable {
 
+    @Column(name = "id_produit")
     private Integer idProduit;
+
+    @Column(name = "id_ingredient")
     private Integer idIngredient;
 
     // Default constructor
