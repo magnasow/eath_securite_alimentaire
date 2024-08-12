95d041a275f2d89d971a827ce24b9f0f5b86e067
magnasow <sowmarie292@gmail.com>
2024-08-12 15:59:37 +0200

    Add files via upload

---
+++ backend/src/main/java/com/eath/dao/UtilisateursRepository.java
@@ -0,0 +1,8 @@
+package com.eath.dao;
+
+import com.eath.entite.Utilisateurs;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UtilisateursRepository extends JpaRepository<Utilisateurs, Integer> {
+
+}
