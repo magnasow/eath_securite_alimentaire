f9522daee78beeffdc5b400ccfa5f5a85b1e8e0a
magnasow <sowmarie292@gmail.com>
2024-08-13 13:25:00 +0200

    Delete backend/src/main/java/com/eath/dao directory

--- backend/src/main/java/com/eath/dao/CommentaireRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eath.dao;
-
-import com.eath.entite.Commentaire;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommentaireRepository extends JpaRepository<Commentaire, Integer> {
-}
