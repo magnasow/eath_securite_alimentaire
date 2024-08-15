ad3fb37582ee21a04eb64c3bd8f589a188a7c7e0
magnasow <sowmarie292@gmail.com>
2024-08-15 14:55:42 +0200

    Add files via upload

---
+++ backend/src/main/java/com/eath/web/LoginRequest.java
@@ -0,0 +1,9 @@
+package com.eath.web;
+
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    private String username;
+    private String password;
+}
---
+++ backend/src/main/java/com/eath/web/UserController.java
@@ -0,0 +1,29 @@
+package com.eath.web;
+
+import com.eath.Service.UserService;
+import com.eath.entite.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        User registeredUser = userService.registerUser(user);
+        return ResponseEntity.ok(registeredUser);
+    }
+
+    @GetMapping("/{username}")
+    public ResponseEntity<User> getUserByUsername(@PathVariable String username) {
+        Optional<User> user = userService.getUserByUsername(username);
+        return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+}
