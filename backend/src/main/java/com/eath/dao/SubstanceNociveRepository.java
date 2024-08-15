acd221646db6214bfff075d1e9edff590449ecb4
magnasow <sowmarie292@gmail.com>
2024-08-15 14:40:14 +0200

    Delete backend/src/main/java/com/eath/dao directory

--- backend/src/main/java/com/eath/dao/SubstanceNociveRepository.java
+++
@@ -1,7 +0,0 @@
-package com.eath.dao;
-
-import com.eath.entite.SubstanceNocive;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubstanceNociveRepository extends JpaRepository<SubstanceNocive, Integer> {
-}
