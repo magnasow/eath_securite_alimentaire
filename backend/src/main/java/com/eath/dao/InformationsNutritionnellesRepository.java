e6e0a0b6279046052a12a25c3f56766c998f017b
magnasow <sowmarie292@gmail.com>
2024-08-16 16:03:58 +0200

    Delete backend/src/main/java/com/eath/dao directory

--- backend/src/main/java/com/eath/dao/InformationsNutritionnellesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eath.dao;
-
-import com.eath.entite.InformationsNutritionnelles;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InformationsNutritionnellesRepository extends JpaRepository<InformationsNutritionnelles, Integer> {
-}
