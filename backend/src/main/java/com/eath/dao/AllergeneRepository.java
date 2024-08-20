d8ba69f72701f44870be2cd8645ccbdb5ace2a03
magnasow <sowmarie292@gmail.com>
2024-08-20 22:55:01 +0200

    Delete backend/src/main/java/com/eath/dao directory

--- backend/src/main/java/com/eath/dao/AllergeneRepository.java
+++
@@ -1,7 +0,0 @@
-package com.eath.dao;
-
-import com.eath.entite.Allergene;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AllergeneRepository extends JpaRepository<Allergene, Integer> {
-}
--- backend/src/main/java/com/eath/dao/PersonneRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eath.dao;
-
-import com.eath.entite.Personne;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonneRepository extends JpaRepository<Personne, Integer> {
-}
