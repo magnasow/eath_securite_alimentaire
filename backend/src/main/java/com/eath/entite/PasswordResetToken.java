19417463c09d42dd1682854e025fac7feaa8cd71
magnasow <sowmarie292@gmail.com>
2024-09-01 21:44:48 +0200

    Add files via upload

--- backend/src/main/java/com/eath/entite/PasswordResetToken.java
+++ backend/src/main/java/com/eath/entite/PasswordResetToken.java
@@ -3,16 +3,11 @@ package com.eath.entite;
 import jakarta.persistence.*;
 import lombok.Data;
 
-
 import java.sql.Timestamp;
-import java.util.Calendar;
-import java.util.Date;
 
 @Entity
 @Data
 public class PasswordResetToken {
-
-    private static final int EXPIRATION = 60 * 24; // Token valide pour 24 heures
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,32 +15,24 @@ public class PasswordResetToken {
 
     private String token;
 
-    @OneToOne(targetEntity = Utilisateurs.class, fetch = FetchType.EAGER)
-    @JoinColumn(nullable = false, name = "user_id")
+    private String verificationCode;
+
+    @ManyToOne
+    @JoinColumn(name = "utilisateur_id")
     private Utilisateurs utilisateur;
 
-    private Date expiryDate;
+    private Timestamp expiryDate;
 
-    public PasswordResetToken() {
-        super();
-    }
+    // Constructeurs, getters, et setters
 
-    public PasswordResetToken(final String token, final Utilisateurs utilisateur) {
-        super();
+    public PasswordResetToken() {}
+
+    public PasswordResetToken(String token, String verificationCode, Utilisateurs utilisateur) {
         this.token = token;
+        this.verificationCode = verificationCode;
         this.utilisateur = utilisateur;
-        this.expiryDate = calculateExpiryDate(EXPIRATION);
+        this.expiryDate = new Timestamp(System.currentTimeMillis() + 3600000); // 1 heure
     }
 
-    private Date calculateExpiryDate(final int expiryTimeInMinutes) {
-        final Calendar cal = Calendar.getInstance();
-        cal.setTimeInMillis(new Date().getTime());
-        cal.add(Calendar.MINUTE, expiryTimeInMinutes);
-        return new Date(cal.getTime().getTime());
-    }
-
-    public void updateToken(final String token) {
-        this.token = token;
-        this.expiryDate = calculateExpiryDate(EXPIRATION);
-    }
+    // Getters et Setters
 }
