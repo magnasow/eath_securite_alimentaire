93da12dd482c819de49cb2d0883eb89989fe3511
magnasow <sowmarie292@gmail.com>
2024-09-02 20:46:04 +0200

    Update PasswordResetService.java

--- backend/src/main/java/com/eath/Service/PasswordResetService.java
+++ backend/src/main/java/com/eath/Service/PasswordResetService.java
@@ -1,8 +1,8 @@
 package com.eath.Service;
 
-
 public interface PasswordResetService {
-    void initiatePasswordReset(String email);
-    void resetPassword(String token, String verificationCode, String newPassword);  // Assurez-vous que les paramètres sont corrects
-}
 
+    void initiatePasswordReset(String email);
+
+    void resetPassword(String token, String newPassword);
+}
