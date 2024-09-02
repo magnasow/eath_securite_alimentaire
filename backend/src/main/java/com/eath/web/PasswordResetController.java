0f3cee90c7ed300bc94aaf1faf833973311096e3
magnasow <sowmarie292@gmail.com>
2024-09-02 20:48:20 +0200

    Update PasswordResetController.java

--- backend/src/main/java/com/eath/web/PasswordResetController.java
+++ backend/src/main/java/com/eath/web/PasswordResetController.java
@@ -3,10 +3,7 @@ package com.eath.web;
 import com.eath.Service.PasswordResetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/password-reset")
@@ -16,17 +13,14 @@ public class PasswordResetController {
     private PasswordResetService passwordResetService;
 
     @PostMapping("/initiate")
-    public ResponseEntity<String> initiatePasswordReset(@RequestParam String email) {
-        passwordResetService.initiatePasswordReset(email);
+    public ResponseEntity<String> initiatePasswordReset(@RequestBody PasswordResetRequest request) {
+        passwordResetService.initiatePasswordReset(request.getEmail());
         return ResponseEntity.ok("Code de vérification envoyé par e-mail");
     }
 
     @PostMapping("/reset")
-    public ResponseEntity<String> resetPassword(@RequestParam String token,
-                                                @RequestParam String verificationCode,
-                                                @RequestParam String newPassword) {
-        passwordResetService.resetPassword(token, verificationCode, newPassword);
+    public ResponseEntity<String> resetPassword(@RequestBody PasswordResetRequest request) {
+        passwordResetService.resetPassword(request.getToken(), request.getNewPassword());
         return ResponseEntity.ok("Mot de passe réinitialisé avec succès");
     }
 }
-
