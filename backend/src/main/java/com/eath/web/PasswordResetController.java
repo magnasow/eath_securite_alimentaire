3c37f79bb8e04cc06866f3ba4b9cc696f764c30d
magnasow <sowmarie292@gmail.com>
2024-09-06 19:46:08 +0200

    Update PasswordResetController.java

--- backend/src/main/java/com/eath/web/PasswordResetController.java
+++ backend/src/main/java/com/eath/web/PasswordResetController.java
@@ -2,9 +2,13 @@ package com.eath.web;
 
 import com.eath.Service.PasswordResetService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
+@CrossOrigin
 @RestController
 @RequestMapping("/api/password-reset")
 public class PasswordResetController {
@@ -13,14 +17,22 @@ public class PasswordResetController {
     private PasswordResetService passwordResetService;
 
     @PostMapping("/initiate")
-    public ResponseEntity<String> initiatePasswordReset(@RequestBody PasswordResetRequest request) {
-        passwordResetService.initiatePasswordReset(request.getEmail());
-        return ResponseEntity.ok("Code de vérification envoyé par e-mail");
+    public ResponseEntity<String> initiatePasswordReset(@Valid @RequestBody PasswordResetRequest request) {
+        boolean success = passwordResetService.initiatePasswordReset(request.getEmail());
+        if (success) {
+            return ResponseEntity.ok("Un lien de réinitialisation de mot de passe a été envoyé par e-mail.");
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erreur : L'utilisateur n'existe pas.");
+        }
     }
 
     @PostMapping("/reset")
-    public ResponseEntity<String> resetPassword(@RequestBody PasswordResetRequest request) {
-        passwordResetService.resetPassword(request.getToken(), request.getNewPassword());
-        return ResponseEntity.ok("Mot de passe réinitialisé avec succès");
+    public ResponseEntity<String> resetPassword(@Valid @RequestBody PasswordResetRequest request) {
+        boolean success = passwordResetService.resetPassword(request.getToken(), request.getNewPassword());
+        if (success) {
+            return ResponseEntity.ok("Le mot de passe a été réinitialisé avec succès.");
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erreur : Jeton invalide ou expiré.");
+        }
     }
 }
