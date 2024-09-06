7a78cc2aca30af96e9b19f6c5fc36b2d639c5f0e
magnasow <sowmarie292@gmail.com>
2024-09-06 19:46:33 +0200

    Update PasswordResetRequest.java

--- backend/src/main/java/com/eath/web/PasswordResetRequest.java
+++ backend/src/main/java/com/eath/web/PasswordResetRequest.java
@@ -2,9 +2,18 @@ package com.eath.web;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
 @Data
 public class PasswordResetRequest {
-    private String email; // Pour initier la réinitialisation
-    private String token; // Pour réinitialiser le mot de passe
-    private String newPassword; // Pour réinitialiser le mot de passe
+    @Email(message = "Adresse e-mail invalide")
+    @NotEmpty(message = "L'email ne peut pas être vide")
+    private String email; // L'adresse e-mail de l'utilisateur
+
+    @NotEmpty(message = "Le jeton ne peut pas être vide")
+    private String token; // Le jeton de réinitialisation envoyé par email
+
+    @NotEmpty(message = "Le nouveau mot de passe ne peut pas être vide")
+    private String newPassword; // Nouveau mot de passe
 }
