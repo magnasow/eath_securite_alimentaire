ceee08dd42c4db4cfd8e66b351ba41374e0af3f1
magnasow <sowmarie292@gmail.com>
2024-12-24 13:57:32 +0100

    Delete frontend directory

--- frontend/lib/connexion_inscription/config.dart
+++
@@ -1,3 +0,0 @@
-class Config {
-  static const String baseUrl = 'https://votre-backend-url.com/api'; // Remplacez par l'URL de votre backend
-}
--- frontend/lib/mes_constantes.dart
+++
@@ -1,5 +0,0 @@
- import 'dart:ui';
-
-
-Color primaryColor=Color(0xFF916CAF);
-Color secondaryColor=Color(0xFF7DC243);
